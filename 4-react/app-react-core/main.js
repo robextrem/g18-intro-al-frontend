e63c079b376c25bf1cc7060d873cb8b68541c1dd
Victor Torres Valencia <victor.tv@wizeline.com>
2022-10-20 21:30:44 -0500

    Create app without JSX

--- 4-react/app-react-core/main.js
+++ 4-react/app-react-core/main.js
@@ -1,9 +1,36 @@
 // MANIPULACION DEL DOM
 
 const root = document.getElementById("div-root");
-const newElement = document.createElement("div");
+// const newElement = document.createElement("div");
 
-newElement.textContent = "Hello world DOM ";
-newElement.className = "container";
+// newElement.textContent = "Hello world DOM ";
+// newElement.className = "container";
 
-root.appendChild(newElement);
+// root.appendChild(newElement);
+
+/** MANIPULACION CON REACT */
+
+const elementReact = React.createElement("span", {}, "Hello world con REACT");
+
+// React.createElement( type = HTML, { AGREGANDO NODO HIJO}, 'text' || null  )
+const secondElementReact = React.createElement("div", {
+  children: React.createElement(
+    "h5",
+    {
+      children: React.createElement(
+        "span",
+        {
+          children: React.createElement(),
+          className: "style-span",
+        },
+        "este es el span"
+      ),
+      className: "h5-span",
+    },
+    "Este es un <H5> dentro de un div"
+  ),
+});
+
+ReactDOM.render(secondElementReact, root);
+
+// NO TIENE JSX
