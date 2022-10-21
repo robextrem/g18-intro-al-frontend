6e088bb5bfc094aed40b3dbdd826b8525874df70
Victor Torres Valencia <victor.tv@wizeline.com>
2022-10-20 20:08:23 -0500

    Create html and js

---
+++ 4-react/app-react-core/explicacion_tmp.js
@@ -0,0 +1,32 @@
+/**
+ * CICLO DE VIDA
+ *
+ * componentDidMount            - 1 VEZ
+ * componentDidUpdate           - N VECES (props || states)
+ * componentWillUnmount         - 1 VEZ
+ */
+
+/**
+ * COMPONENTES
+ *              props
+ *              state
+ */
+
+/**
+ * FORMAS DE TRABAJAR DE REACT
+ *
+ * CLASS COMPONENTS   ✅ (CICLO DE VIDA 👆🏽) VIEJITO
+ * FUNCTIONAL COMPONENTS (HOOKS) NUEVA - COOL
+ */
+
+/**
+ * JSX - syntax extension to JS
+ *
+ * HTML + JS 😮
+ *
+ * const returnH5 = ()  => {                JS
+ *  return <p>                              HTML
+ *          <div> <span> HOLA amigs </span> </div>
+ *         </p>
+ * }
+ */
