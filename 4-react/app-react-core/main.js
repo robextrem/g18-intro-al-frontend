da86f50742a249420bf3f585c89b00b79e572029
Victor Torres Valencia <victor.tv@wizeline.com>
2022-10-20 20:29:33 -0500

    Adde element with DOM

--- 4-react/app-react-core/main.js
+++ 4-react/app-react-core/main.js
@@ -1,1 +1,9 @@
-console.log("react");
+// MANIPULACION DEL DOM
+
+const root = document.getElementById("div-root");
+const newElement = document.createElement("div");
+
+newElement.textContent = "Hello world DOM ";
+newElement.className = "container";
+
+root.appendChild(newElement);
