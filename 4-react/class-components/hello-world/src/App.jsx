73b04c0d7cb22bf1b83893ab66ef72b8edfba7ea
Victor Torres Valencia <victor.tv@wizeline.com>
2022-10-24 20:46:15 -0500

    Added styles in JSX

--- 4-react/class-components/hello-world/src/App.jsx
+++ 4-react/class-components/hello-world/src/App.jsx
@@ -25,23 +25,38 @@ class App extends Component {
       );
     };
 
+    const styleObject = {
+      fontSize: "14px",
+      borderStyle: "none",
+    };
+
     // 3.- Regresar JSX
     return (
       <div className="App">
-        <h5>Hola mundo</h5>
+        <h5>Hola mundo </h5>
 
-        <div className="card">
+        {/* ESTILOS Se maneja através de objetos
+        Se usa camelCase */}
+        <div className="card" style={{ fontSize: 35, borderStyle: "inset" }}>
           {/* SINTAXIS para acceder a valores de JS {} */}
           <span>Esta es la generacion {name} </span>
           <h4>& hoy es el dia {day}</h4>
           {greetings()}
+
           {/* para setear estados se usa 
             this.setState({ name: newValue}) 
           */}
+          <p>El valor actual del contador es: {this.state.count}</p>
           <button
             onClick={() => this.setState({ count: this.state.count + 1 })}
           >
-            valor de contador is {this.state.count}
+            Incrementar
+          </button>
+
+          <button
+            onClick={() => this.setState({ count: this.state.count - 1 })}
+          >
+            Decrementar
           </button>
         </div>
       </div>
