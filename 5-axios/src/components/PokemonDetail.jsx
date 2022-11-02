e87adec12e5c0da6b033ac1eeb74fcf81e3a1c29
Victor Torres Valencia <victor.tv@wizeline.com>
2022-11-01 20:35:18 -0600

    .

--- 5-axios/src/components/PokemonDetail.jsx
+++ 5-axios/src/components/PokemonDetail.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 export default class PokemonDetail extends Component {
   componentWillUnmount() {
-    alert("se destruye componente");
+    console.log("se destruye componente");
   }
 
   render() {
