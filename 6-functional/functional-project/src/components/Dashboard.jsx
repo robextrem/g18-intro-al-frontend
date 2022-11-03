53d3084c5b9eda10335fc4c3738230ea13072b16
Victor Torres Valencia <victor.tv@wizeline.com>
2022-11-02 18:54:44 -0600

    Implement SWAPI when COMPONENT MOUNT

--- 6-functional/functional-project/src/components/Dashboard.jsx
+++ 6-functional/functional-project/src/components/Dashboard.jsx
@@ -1,6 +1,24 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect } from "react";
 
 export default function Dashboard() {
+  useEffect(() => {
+    // consumir la API de star wars
+    const fetchApi = async () => {
+      const BASE_URL = "https://swapi.dev/api/";
+      try {
+        const result = await axios.get(`${BASE_URL}people`);
+        console.log("result", result);
+      } catch (error) {
+        console.log("error", error);
+      }
+    };
+
+    fetchApi();
+
+    // PROMESA THEN CATCH 👉🏽 axios.get(`${BASE_URL}people`).then().catch()
+  }, []);
+
   return (
     <>
       <div>Dashboard</div>
