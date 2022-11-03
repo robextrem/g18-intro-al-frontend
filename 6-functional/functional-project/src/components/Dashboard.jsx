a3e14d9ef63868be8d62b03bb314c0e708e26886
Victor Torres Valencia <victor.tv@wizeline.com>
2022-11-02 19:07:55 -0600

    Iterate charactersArray

--- 6-functional/functional-project/src/components/Dashboard.jsx
+++ 6-functional/functional-project/src/components/Dashboard.jsx
@@ -1,28 +1,44 @@
 import axios from "axios";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 export default function Dashboard() {
+  const [charactersArray, setCharactersArray] = useState([]);
+
   useEffect(() => {
-    // consumir la API de star wars
     const fetchApi = async () => {
       const BASE_URL = "https://swapi.dev/api/";
       try {
-        const result = await axios.get(`${BASE_URL}people`);
-        console.log("result", result);
+        const {
+          data: { results },
+        } = await axios.get(`${BASE_URL}people`);
+        console.log("results", results);
+        setCharactersArray(results);
       } catch (error) {
         console.log("error", error);
       }
     };
 
     fetchApi();
-
     // PROMESA THEN CATCH 👉🏽 axios.get(`${BASE_URL}people`).then().catch()
   }, []);
 
+  // quiero visualizar en un console.log cada que cambie el estado charactersArray  👀
+  useEffect(() => {
+    console.log("cambia estado", charactersArray);
+  }, [charactersArray]);
+
   return (
     <>
-      <div>Dashboard</div>
-      <h4>Bienvenido</h4>
+      <h4>Dashboard</h4>
+
+      <h5>Lista de personajes </h5>
+      {charactersArray.map((element, i) => (
+        <div key={i}>
+          <p>Name: {element.name}</p>
+          <p>Height: {element.height}</p>
+          <p>Hair color: {element.hair_color}</p>
+        </div>
+      ))}
     </>
   );
 }
