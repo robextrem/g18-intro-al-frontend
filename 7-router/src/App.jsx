57ae41bef2980d6a578e622e78a4ff7ae38bbcc6
Roberto Romero <roberto@ulama.co>
2022-11-03 20:50:41 -0600

    links

--- 7-router/src/App.jsx
+++ 7-router/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate, Link } from "react-router-dom";
 import "./App.css";
 import Dashboard from "./components/Dashboard";
 import Login from "./components/Login";
@@ -7,16 +7,24 @@ import User from "./components/User";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="login" element={<Navigate to="/" replace />} />
-      {/* IMPORTANTE poner  👉🏽 / */}
-      <Route path="main-dashboard" element={<Dashboard />} />
-      <Route path="user" element={<User />} />
+    <>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="login" element={<Navigate to="/" replace />} />
+        {/* IMPORTANTE poner  👉🏽 / */}
+        <Route path="main-dashboard" element={<Dashboard />} />
+        <Route path="user" element={<User />} />
 
-      {/* pagina 404 */}
-      <Route path="*" element={<h3> Page not found 404 </h3>} />
-    </Routes>
+        {/* pagina 404 */}
+        <Route path="*" element={<h3> Page not found 404 </h3>} />
+      </Routes>
+       <div>
+        <Link className="boton" to="/">Home</Link>
+        <Link className="boton" to="login">Login</Link>
+        <Link className="boton" to="main-dashboard">Dashboard</Link>
+        <Link className="boton" href="/user">User</Link>
+       </div>
+    </>
   );
 }
 
