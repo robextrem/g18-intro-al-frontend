6403814b340bc1db8f6d953569b2930d027f87e0
Victor Torres Valencia <victor.tv@wizeline.com>
2022-11-03 20:12:55 -0600

    Added navigate router

--- 7-router/src/App.jsx
+++ 7-router/src/App.jsx
@@ -3,13 +3,16 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import "./App.css";
 import Dashboard from "./components/Dashboard";
 import Login from "./components/Login";
+import User from "./components/User";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
+      <Route path="login" element={<Navigate to="/" replace />} />
       {/* IMPORTANTE poner  👉🏽 / */}
-      <Route path="/main-dashboard" element={<Dashboard />} />
+      <Route path="main-dashboard" element={<Dashboard />} />
+      <Route path="user" element={<User />} />
 
       {/* pagina 404 */}
       <Route path="*" element={<h3> Page not found 404 </h3>} />
